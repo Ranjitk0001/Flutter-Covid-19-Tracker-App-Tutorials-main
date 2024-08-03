c87c92dfd7a0ec68a4c1794d630cdf98ae59ba12
rk <ranjitkale0001@gmail.com>
2024-08-03 20:52:08 +0530

    not complete
    incomple to github dowload stages

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children:  [
-
+                    Text('Covid data')
           ],
         ),
       ),
